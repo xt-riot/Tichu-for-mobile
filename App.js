43796723e37cc8b81401862b760099c53f5f93c8
Konstantinos Karachristos <karachris@csd.auth.gr>
2020-10-23 07:07:32 +0300

    splitted some code to multiple .js sources

--- App.js
+++ App.js
@@ -2,59 +2,12 @@ import React, { useState, useRef } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
-  TouchableOpacity,
-  FlatList
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const CustomButton = ({ onPress, title, size, color }) => {
-  return (
-  <TouchableOpacity onPress={onPress} style={{
-    flex: 1,
-    margin: 1,
-    backgroundColor: '#009688',
-    justifyContent: 'center',
-  }}>
-    <Text style={[{
-                    color: 'white',
-                    padding:7,
-                    fontWeight: 'bold',
-                    alignSelf:'center',
-                    textTransform: 'uppercase'
-                  },
-                    color && { color }
-                ]}>
-        {title}
-    </Text>
-  </TouchableOpacity>
-  );
-};
-
-const Item = ({ teamA, teamB, show }) => (
-  <View style = {{flex: 1, flexDirection: 'row', justifyContent:'center', alignContent:'center', alignItems:'center'}}>
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text style={{color: 'white'}}>{(show > 0)?teamA:''}</Text>
-    </View>
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <Text style={{color: 'white'}}>{(show > 0)?teamB:''}</Text>
-    </View>
-  </View>
-);
-
-const renderItem = ({ item, index }) => (
-    <Item teamA={item.teamA} teamB={item.teamB} show={index} />
-);
+import CustomButton from './src/CustomButton';
+import CustomFlatList from './src/FlatList';
 
 const App: () => React$Node = () => {
 
@@ -70,16 +23,50 @@ const App: () => React$Node = () => {
   const [scoreList, setScoreList] = useState([{id: 0, teamA: 0, teamB: 0}]);
   // Total games currently played
   const [totalGames, setTotalGames] = useState(1);
-  
-  const addScoreGT = function(amount, team) {
+
+  const addScoreButton = () => {
+    scoreList[0].teamA += scoreA;
+    scoreList[0].teamB += scoreB;
+    setScoreList([...scoreList, 
+    {
+      id: totalGames,
+      teamA: scoreA,
+      teamB: scoreB,
+    }]);
+    setTotalGames(totalGames+1);
+    setScoreA(0);
+    setScoreB(0);
+    setAllScores([0,0,0,0]);
+  }
+
+  const RemoveRoundButton = () => {
+    scoreList[0].teamA -= scoreList[totalGames-1].teamA;
+            scoreList[0].teamB -= scoreList[totalGames-1].teamB;
+            setScoreList(scoreList.slice(0,-1));
+            setTotalGames(totalGames-1);
+            setScoreA(0);
+            setScoreB(0);
+            setAllScores([0,0,0,0]);
+  }
+
+  const ResetButton = () => {
+    setScoreList([{id: 0, teamA: 0, teamB: 0}]);
+              setScoreA(0);
+              setScoreB(0);
+              setAllScores([0,0,0,0]);
+              setTotalGames(1);
+  }
+
+  const addScoreGT = (amount, team) => {
     if ( allScores[(team * 2) + 1] + amount < 201 && allScores[(team * 2) + 1] + amount > -301) {
       allScores[(team * 2) + 1] += amount;
       setScoreA(allScores[0] + allScores[1]);
       setScoreB(100 - allScores[0] + allScores[3]);
     }
   }
-
-  const addScore = function(amount, team) {
+  
+  
+  const addScore = (amount, team) => {
     let temp = 0;
     if (team == 0)
       temp = 1;
@@ -114,13 +101,7 @@ const App: () => React$Node = () => {
       </View>
 
       <View style={{ flex: 3 }}>
-        <FlatList
-              ref={flatList}
-              data={scoreList}
-              renderItem={renderItem}
-              keyExtractor={(item, index) => index.toString()}
-              onContentSizeChange={() => (flatList.current.scrollToEnd())}
-        />
+        <CustomFlatList scores={scoreList} />
       </View>
 
 
@@ -131,7 +112,9 @@ const App: () => React$Node = () => {
           <CustomButton title='tichu' color='green' onPress={() => {addScoreGT(100, 0)} } />
           <CustomButton title='tichu' color='red' onPress={() => {addScoreGT(-100, 0)} } />
         </View>
+
         <View style={{ flex: 2, marginRight: 5}}>
+        
           <View style={{flex: 1, justifyContent:'center'}}>
             <Text style={styles.scoreText}>
               {scoreA}
@@ -144,17 +127,15 @@ const App: () => React$Node = () => {
           <View style={styles.pureScoreButtons}>
             <CustomButton title='5' onPress={() => {addScore(5, 0)} } />
             <CustomButton title='10' onPress={() => {addScore(10, 0)} } />
-
           </View>
           <View style={styles.pureScoreButtons}>
             <CustomButton title='20' onPress={() => {addScore(20, 0)} } />
             <CustomButton title='50' onPress={() => {addScore(50, 0)} } />
-
           </View>
-
         </View>
+
         <View style={{ flex: 2, marginLeft: 5}}>
-        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <View style={{ flex: 1, justifyContent: 'center' }}>
             <Text style={styles.scoreText}>
               {scoreB}
             </Text>
@@ -166,64 +147,32 @@ const App: () => React$Node = () => {
           <View style={styles.pureScoreButtons}>
             <CustomButton title='5' onPress={() => {addScore(5, 1)} } />
             <CustomButton title='10' onPress={() => {addScore(10, 1)} } />
-
           </View>
           <View style={styles.pureScoreButtons}>
             <CustomButton title='20' onPress={() => {addScore(20, 1)} } />
             <CustomButton title='50' onPress={() => {addScore(50, 1)} } />
-
           </View>
-
         </View>
+
         <View style={{flex: 1}}>
           <CustomButton title='grand' color='green' onPress={() => {addScoreGT(200, 1)} } />
           <CustomButton title='grand' color='red' onPress={() => {addScoreGT(-200, 1)} } />
           <CustomButton title='tichu' color='green' onPress={() => {addScoreGT(100, 1)} } />
           <CustomButton title='tichu' color='red' onPress={() => {addScoreGT(-100, 1)} } />
         </View>
-
       </View>
 
       <View style = {styles.footer}>
         <View style={styles.footerButtons}>
-          <CustomButton title="Add Score" onPress={()=> {
-            scoreList[0].teamA += scoreA;
-            scoreList[0].teamB += scoreB;
-            setScoreList([...scoreList, 
-            {
-              id: totalGames,
-              teamA: scoreA,
-              teamB: scoreB,
-            }]);
-            setTotalGames(totalGames+1);
-            setScoreA(0);
-            setScoreB(0);
-            setAllScores([0,0,0,0]);
-          }} />
+          <CustomButton title="Add Score" onPress={()=> { addScoreButton(); }} />
         </View>
         <View style={styles.footerButtons}>
-          <CustomButton title='Remove last round' onPress={() => {
-            scoreList[0].teamA -= scoreList[totalGames-1].teamA;
-            scoreList[0].teamB -= scoreList[totalGames-1].teamB;
-            setScoreList(scoreList.slice(0,-1));
-            setTotalGames(totalGames-1);
-            setScoreA(0);
-            setScoreB(0);
-            setAllScores([0,0,0,0]);
-          }}/>
-          </View>
-          <View style={styles.footerButtons}>
-            <CustomButton title='reset' onPress={() => {
-              setScoreList([{id: 0, teamA: 0, teamB: 0}]);
-              setScoreA(0);
-              setScoreB(0);
-              setAllScores([0,0,0,0]);
-              setTotalGames(1);
-            }} />
-
-          </View>
+          <CustomButton title='Remove last round' onPress={() => { RemoveRoundButton(); }}/>
         </View>
-
+        <View style={styles.footerButtons}>
+          <CustomButton title='reset' onPress={() => { ResetButton(); }} />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -253,26 +202,26 @@ const styles = StyleSheet.create({
     flex: 2,
     flexDirection: 'row'
   },
+  footerButtons: {
+    marginLeft: 20,
+    marginTop: 10,
+    marginRight: 20
+  },
   pureScoreButtons: { 
     flex: 1,
     flexDirection: 'row' 
-},
-scoreText: {
-  color:'white',
-  justifyContent:'center',
-  alignSelf: 'center'
-},
-footerButtons: {
-  marginLeft: 20,
-  marginTop: 10,
-  marginRight: 20
-},
-footer: {
-  flex: 0.4,
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'row', 
-}
+  },
+  scoreText: {
+      color:'white',
+      justifyContent:'center',
+      alignSelf: 'center'
+  },
+  footer: {
+    flex: 0.4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row', 
+  }
 
 });
 
